a590b572c83d8852c439f9adad6d164223316f94
tnkang-205 <khangnhut18@gmail.com>
2026-05-29 00:50:22 +0700

    Delete netlify directory

--- netlify/functions/auth.js
+++
@@ -1,8 +0,0 @@
-const { GithubOAuthProvider, OAuthProvider } = require('@decap-cms/netlify-cms-oauth-provider-node');
-
-const provider = new GithubOAuthProvider({
-  clientId: process.env.GITHUB_CLIENT_ID,
-  clientSecret: process.env.GITHUB_CLIENT_SECRET,
-});
-
-exports.handler = OAuthProvider.handler(provider);
